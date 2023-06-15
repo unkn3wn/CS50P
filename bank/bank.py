e07e95457eb7f3246248dc0381b8ee3e83a4f1c6
unkn3wn <reyesfernando151@gmail.com>
2023-06-14 20:58:53 -0700

    extensions

---
+++ bank/bank.py
@@ -0,0 +1,8 @@
+str = input("Greeting:" ).lower().strip()
+print(str)
+if str == "hello" or str =="hello, newman":
+    print("$0")
+elif str == "how you doing?":
+    print("$20")
+else:
+    print("$100")
